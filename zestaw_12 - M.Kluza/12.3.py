40780268fb0900a33c2c35d22b0b6d681dbf7b8b
mariusz-kluza <49817130+mariusz-kluza@users.noreply.github.com>
2020-01-21 17:02:16 +0100

    Update 12.3.py

--- zestaw_12 - M.Kluza/12.3.py
+++ zestaw_12 - M.Kluza/12.3.py
@@ -10,15 +10,16 @@ def list_random_repeated(n):
 
 
 def mediana_sort(L, left, right):
-    K = sorted(L)
-    s = len(K)
-    if s%2 == 0:
-        m = (K[s//2] + K[(s//2)-1])/2
+    list_range = L[left:right+1]
+    list_range_sorted = sorted(list_range)
+    length = len(list_range_sorted)
+    if length%2 == 0:
+        median = (list_range_sorted[length//2] + list_range_sorted[(length//2)-1])/2
     else:
-        m = K[s//2]
+        median = list_range_sorted[length//2]
 
-    print("W liscie L: " + str(L) + "\nmediana wynosi: " + str(m) + ".")
+    print("W liscie L:", str(L), "w zakresie od", str(left), "do", str(right) + ",\nmediana wynosi: ", str(median) + ".")
 
 
 L = list_random_repeated(10)
-mediana_sort(L, 0, len(L)-1)
+mediana_sort(L, 6, len(L)-1)
